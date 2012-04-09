fcb623c1289ec63947765ddaac3fbc5f572af20b
Konrad Krol <konrad.krol89@gmail.com>
2012-04-09 21:15:29 +0200

    Dodano DomainService i DataClasses do modulu CarsManagement

---
+++ CarsManagement.Ria.Web/CarsManagementDomainService.cs
@@ -0,0 +1,50 @@
+﻿
+namespace CarsManagement.Ria.Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Data.Linq;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+    using Microsoft.ServiceModel.DomainServices.LinqToSql;
+
+
+    // Implements application logic using the CarsManagementDataClassesDataContext context.
+    // TODO: Add your application logic to these methods or in additional methods.
+    // TODO: Wire up authentication (Windows/ASP.NET Forms) and uncomment the following to disable anonymous access
+    // Also consider adding roles to restrict access as appropriate.
+    // [RequiresAuthentication]
+    [EnableClientAccess()]
+    public class CarsManagementDomainService : LinqToSqlDomainService<CarsManagementDataClassesDataContext>
+    {
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        public IQueryable<samochody> GetSamochodies()
+        {
+            return this.DataContext.samochodies;
+        }
+
+        public void InsertSamochody(samochody samochody)
+        {
+            this.DataContext.samochodies.InsertOnSubmit(samochody);
+        }
+
+        public void UpdateSamochody(samochody currentsamochody)
+        {
+            this.DataContext.samochodies.Attach(currentsamochody, this.ChangeSet.GetOriginal(currentsamochody));
+        }
+
+        public void DeleteSamochody(samochody samochody)
+        {
+            this.DataContext.samochodies.Attach(samochody);
+            this.DataContext.samochodies.DeleteOnSubmit(samochody);
+        }
+    }
+}
+
+
---
+++ CarsManagement.Ria.Web/CarsManagementDomainService.metadata.cs
@@ -0,0 +1,53 @@
+﻿
+namespace CarsManagement.Ria.Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+
+
+    // The MetadataTypeAttribute identifies samochodyMetadata as the class
+    // that carries additional metadata for the samochody class.
+    [MetadataTypeAttribute(typeof(samochody.samochodyMetadata))]
+    public partial class samochody
+    {
+
+        // This class allows you to attach custom attributes to properties
+        // of the samochody class.
+        //
+        // For example, the following marks the Xyz property as a
+        // required property and specifies the format for valid values:
+        //    [Required]
+        //    [RegularExpression("[A-Z][A-Za-z0-9]*")]
+        //    [StringLength(32)]
+        //    public string Xyz { get; set; }
+        internal sealed class samochodyMetadata
+        {
+
+            // Metadata classes are not meant to be instantiated.
+            private samochodyMetadata()
+            {
+            }
+
+            public bool ac { get; set; }
+
+            public int id { get; set; }
+
+            public string marka { get; set; }
+
+            public bool nw { get; set; }
+
+            public bool oc { get; set; }
+
+            public string opis { get; set; }
+
+            public short rok_produkcji { get; set; }
+
+            public string status_ { get; set; }
+        }
+    }
+}
